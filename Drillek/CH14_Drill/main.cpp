6dff475a1eb012b989b9de8659d2cc55cd09ab64
Boldi247 <kovacs.boldizsar0321@gmail.com>
2021-03-11 12:10:04 +0100

    minor fixes

--- Drillek/CH14_Drill/main.cpp
+++ Drillek/CH14_Drill/main.cpp
@@ -1,21 +1,21 @@
 #include "std_lib_facilities.h"
 
-struct A {
-	virtual void pvf() const = 0;
-}
-
-struct B1 : A {
-	virtual void vf() {cout << "B1::vf()\n";}
-	void f() {cout << "B1::f()\n";}
-	void pvf() {cout << "B::pvf()\n";}
+struct A{
+	virtual void pvf() = 0;
 };
 
-struct D1 : B1 {
+struct B1 : A{
+	virtual void vf() {cout << "B1::vf()\n";}
+	void f() {cout << "B1::f()\n";}
+	void pvf() {cout << "B1::pfv()\n";}
+};
+
+struct D1 : B1{
 	void vf() override {cout << "D1::vf()\n";}
 	void f() {cout << "D1::f()\n";}
 };
 
-struct D2 : D1 {
+struct D2 : D1{
 	void pvf() override {cout << "D2::pvf()\n";}
 };
 
@@ -24,14 +24,20 @@ int main()
 	B1 b1;
 	b1.vf();
 	b1.f();
+	b1.pvf();
+	cout << endl;
 
 	D1 d1;
 	d1.vf();
 	d1.f();
-	
-	B1& b1ref = d1; //"r" as reference
-	b1ref.vf();
-	b1ref.f();
+	d1.pvf();
+	cout << endl;
+
+	B1& b1_ref {d1};
+	b1_ref.vf();
+	b1_ref.f();
+	b1_ref.pvf();
+	cout << endl;
 
 	D2 d2;
 	d2.vf();
